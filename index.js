2670c294da9b6b03f6eeb9fb1f84dbee8cbdd365
tobnys <tobnystrom@gmail.com>
2017-07-31 23:55:41 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.send("ok");
+});
+
+app.listen(8080, () => console.log("Running on port 8080"));
